e6a64556d75fbb72c21072e373f2fedc79cb9173
anton_vakhrushin <anton_vakhrushin@mail.ru>
2018-12-16 19:27:47 +0300

    Create nodejs backend with mongodb DataBase and monk, joi

---
+++ server/src/api/messages.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const Joi = require('joi');
+
+const db = require('../db');
+const messages = db.get('messages');
+
+const router = express.Router();
+
+const schema = Joi.object().keys({
+    name: Joi.string().alphanum().min(1).max(50).required(),
+    message: Joi.string().min(1).max(500).required(),
+    latitude: Joi.number().min(-90).max(90).required(),
+    longitude: Joi.number().min(-180).max(180).required(),
+});
+
+router.get('/', (req, res) => {
+  res.json([]);
+});
+
+router.post('/', (req, res, next) => {
+    const result = Joi.validate(req.body, schema);
+    if (result.error === null) {
+        // add current time
+        // insert into DB
+        const { name, message, latitude, longitude } = req.body;
+        const userMessage = {
+            name,
+            message,
+            latitude,
+            longitude,
+            date: new Date()
+        };
+        messages
+            .insert(userMessage)
+            .then(insertedMessage => {
+                res.json(insertedMessage);
+            });
+    } else {
+        next(result.error);
+    }
+});
+
+
+module.exports = router;
